6be2b35507856665721ff33e0c866d99ef8c65a0
Arpitmovers <garg.arpit504@gmail.com>
2024-10-23 07:22:02 +0530

    added BigBark

--- go.mod
+++ go.mod
@@ -1,4 +1,5 @@
 module github.com/Arpitmovers/puppy
 
-
 go 1.22.2
+
+require github.com/Arpitmovers/dog v0.0.0-20241023014528-ada8b62d6dbf // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	"github.com/Arpitmovers/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrowsUp(Barks())
 }
